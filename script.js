3d8840d85b93c439e0fad35afd04e4e6260a5c9c
WaluigisKitten <114946244+WaluigisKitten@users.noreply.github.com>
2023-10-27 10:50:19 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// JavaScript (script.js)
+document.addEventListener("DOMContentLoaded", function() {
+    const spookyButton = document.getElementById("spooky-button");
+    const pumpkinImage = document.querySelector(".pumpkin");
+
+    // Function to trigger a spooky surprise
+    function triggerSpookySurprise() {
+        pumpkinImage.classList.add("rotate");
+        setTimeout(() => {
+            pumpkinImage.classList.remove("rotate");
+        }, 2000);
+    }
+
+    // Add an event listener to the button
+    spookyButton.addEventListener("click", triggerSpookySurprise);
+});
+
+// script.js
+document.getElementById('animated-button').addEventListener('click', function(event) {
+    event.preventDefault(10000); // Prevent the default link behavior
+    
+    // Add an animation class to trigger the animation
+    this.classList.add('animate');
+    
+    // After a delay, open the link
+    setTimeout(() => {
+        window.location.href = this.getAttribute('href');
+    }, 1000); // Adjust the delay (in milliseconds) as needed
+});
